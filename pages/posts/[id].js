4166d11c93b0186db4b62345ec6bc6c1c1aa52b3
Andrés <karxll@gmail.com>
2023-01-09 22:07:13 -0600

    Fix: Fixed og meta: image, description, title

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -28,7 +28,17 @@ export default function Post({ postData }) {
     return (
       <Layout>
         <Head>
-          <title>{postData.title}</title>
+            <title>{postData.title}</title>
+            <meta
+            name="description"
+            content={`${postData.title} | Andrés Parra Developer, CTO para Startups, Head on Engineering`}
+            />
+            <meta
+            property="og:image"
+            content="/images/ogimage.jpg"
+            />
+            <meta name="og:title" content={`${postData.title} | Andrés Parra Developer`} />
+            <meta name="twitter:card" content="summary_large_image" />
         </Head>
         <article>
           <h1 className={utilStyles.headingXl}>{postData.title}</h1>
